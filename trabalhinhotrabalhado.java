e13505bd22ca4d11f201ef29ba9f7422e9191599
André Pereira <1240733@isep.ipp.pt>
2024-11-21 10:27:51 +0000

    Update trabalhinhotrabalhado.java

--- trabalhinhotrabalhado.java
+++ trabalhinhotrabalhado.java
@@ -1,3 +1,24 @@
+import java.lang.reflect.Array;
 public class trabalhinhotrabalhado {
-    
+    //a)
+
+    //b)
+    public static void kilometros() {
+        int soma = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                soma += array[j][i];
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("total de km a percorrer\nv%d :    %d km\n", i, soma);
+        }
+    }
+    //c)
+
+    //d)
+    public static void cargaDasBaterias(){
+        
+    }
+
 }

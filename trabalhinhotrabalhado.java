c018a1ef5700577b2bc6f1b04edeafea74c7ecd4
Tomas Pinho <1240731@isep.ipp.pt>
2024-11-21 10:57:18 +0000

    trabalhei no trabalho trabalhado
    alinea a

--- trabalhinhotrabalhado.java
+++ trabalhinhotrabalhado.java
@@ -1,3 +1,41 @@
+import java.util.Scanner;
+
 public class trabalhinhotrabalhado {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String textDescritivo = sc.nextLine();
+        int numVeiculos = sc.nextInt();
+        int numDias = sc.nextInt();
+        matrizI(lerInfo(numVeiculos, numDias));
+
+    }
+
+    public static int [] [] lerInfo(int numVeiculos, int numDias) {
+
+        Scanner sc= new Scanner(System.in);
+
+        int [][] matriz = new int[numVeiculos][numDias];
+
+        for (int i = 0; i < numVeiculos; i++) {
+            for (int j = 0; j < numDias; j++) {
+                int kmPercorridos = sc.nextInt();
+                matriz[i][j] = kmPercorridos;
+            }
+
+        }
+       return matriz;
+    }
+    public static void matrizI(int [][] matriz) {
+       for (int i = 0; i < matriz.length; i++) {
+           for (int j = 0; j < matriz[i].length; j++) {
+               System.out.print(matriz[i][j] + " ");
+           }
+           System.out.println();
+       }
+
+    }
+
+
 }
